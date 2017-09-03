7e5908d2be6da3563a2ad5fb033b4f64b8cdd4f7
Mazin Ahmed <mazin@protonmail.com>
2017-09-04 01:32:55 +0300

    v0.5 release

--- README.md
+++ README.md
@@ -1,65 +1,66 @@
 *Firefox Security Toolkit*
 ====================
-### A tool that transforms Firefox Browsers into a penetration testing suite ###
+### A tool that transforms Firefox browsers into a penetration testing suite ###
 
 
-## How? ##
+# How? #
 It downloads the most important extensions, and install it on your browser. The used extensions has been chosen by a survey among the information security community. Based on it's results, *Firefox Security Toolkit* was made. Also, it allows you to download Burp Suite certificate and a large user-agent list for User-Agent Switcher. Making it one-click away to prepare your web-application testing browser.
 
-## How does it differ from well-known projects, such as *OWASP Mantra* and *Hcon STF*? ##
+# How does it differ from well-known projects, such as *OWASP Mantra* and *Hcon STF*? #
 *OWASP Mantra* and *Hcon STF* are not regularly updated, and needs a lot of work in order to develop and maintain. Meanwhile, *Firefox Security Toolkit* does not need a additional maintaining, although I would be maintaining it for any issues/bugs if needed. The used extensions are downloaded from Mozilla Addons Store with its latest version, to ensure the best testing experience for the penetration tester.
 
-## Who can use *Firefox Security Toolkit*? ##
+# Who can use *Firefox Security Toolkit*? #
 Web-Application Penetration Testers, Information Security Learners, and basically anyone interested in web-application security.
 
-## Compatibility ##
-The project currently supports Linux/Unix environments. 
+# Compatibility #
+The project supports Linux/Unix environments.
 
-## Usage ##
+# Usage #
 bash ./firefox_security_toolkit.sh
 
-### Demo Video ###
+# Demo Video #
 [Demo Video](https://www.youtube.com/watch?v=0pD-tNrxrzY)
 
-#### Available Addons:- ###
-* Cookie Export/Import
-* Cookie Manager 
-* Copy as Plain Text
-* Crypto Fox
-* CSRF-Finder
-* Disable WebRTC
-* FireBug
-* Fireforce
-* FlagFox
-* Foxy Proxy
-* HackBar
-* Live HTTP Headers
-* Multi Fox
-* PassiveRecon
-* Right-Click XSS
-* Tamper Data
-* User Agent Switcher
-* Wappalyzer
-* Web Developer
+# Available Add-ons #
+* Advanced Dork (https://addons.mozilla.org/en-US/firefox/addon/advanced-dork/)
+* Cookie Export/Import (https://addons.mozilla.org/en-US/firefox/addon/cookies-exportimport/)
+* Cookie Manager+ (https://addons.mozilla.org/en-US/firefox/addon/cookies-manager-plus/)
+* Copy as Plain Text (https://addons.mozilla.org/en-US/firefox/addon/copy-as-plain-text/)
+* Crypto Fox (https://addons.mozilla.org/en-US/firefox/addon/cryptofox/)
+* CSRF-Finder (https://addons.mozilla.org/en-us/firefox/addon/csrf-finder/)
+* Disable WebRTC (https://addons.mozilla.org/en-US/firefox/addon/happy-bonobo-disable-webrtc/)
+* FireBug (https://addons.mozilla.org/en-US/firefox/addon/firebug/)
+* Fireforce (https://addons.mozilla.org/en-US/firefox/addon/fireforce/)
+* FlagFox (https://addons.mozilla.org/en-US/firefox/addon/flagfox/)
+* Foxy Proxy (https://addons.mozilla.org/en-US/firefox/addon/foxyproxy-standard/)
+* Greasemonkey (https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+* HackBar (https://addons.mozilla.org/en-US/firefox/addon/hackbar/)
+* IP Address and Domain Information (https://addons.mozilla.org/en-US/firefox/addon/ip-address-and-domain-info/)
+* JavaScript Deobfuscator (https://addons.mozilla.org/en-US/firefox/addon/javascript-deobfuscator/)
+* Live HTTP Headers (https://addons.mozilla.org/en-US/firefox/addon/live-http-headers-fixed-by-dan/)
+* PassiveRecon (https://addons.mozilla.org/en-US/firefox/addon/passiverecon/)
+* Regular Expressions Tester (https://addons.mozilla.org/en-US/firefox/addon/rext/)
+* Right-Click XSS (https://addons.mozilla.org/en-us/firefox/addon/rightclickxss/)
+* Tamper Data (https://addons.mozilla.org/en-US/firefox/addon/tamper-data/)
+* User Agent Switcher (https://addons.mozilla.org/en-US/firefox/addon/user-agent-switcher/)
+* Wappalyzer (https://addons.mozilla.org/en-US/firefox/addon/wappalyzer/)
+* Web Developer (https://addons.mozilla.org/en-us/firefox/addon/web-developer/)
 
-### Additional Features:- ###
-* Downloading Burp Suite Certificate
-* Downloading a large user-agent list for User-Agent Swithcer
+# Additions & Features #
+* Downloading Burp Suite certificate.
+* Downloading a large user-agent list for User-Agent Switcher.
 
 
-### **Legal Disclaimer** ###
+# **Legal Disclaimer** #
 This project is made for educational and ethical testing purposes only. Usage of Firefox Security Toolkit for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.
 
 
-### **License** ###
+# **License** #
 The project is licensed under MIT License.
 
-### **Author** ###
+# **Author** #
 *Mazin Ahmed*
 * Website: [https://mazinahmed.net](https://mazinahmed.net)
 * Email: *mazin AT mazinahmed DOT net*
 * Twitter: [https://twitter.com/mazen160](https://twitter.com/mazen160)
 * Linkedin: [http://linkedin.com/in/infosecmazinahmed](http://linkedin.com/in/infosecmazinahmed)
-
-
-
--- firefox_security_toolkit.sh
+++ firefox_security_toolkit.sh
@@ -1,185 +1,208 @@
 #!/bin/bash
-##################################################################################
-##Firefox Security Toolkit
-###Description:
-#This script automatically transform Firefox Browser to a penetration testing suite. The script mainly focuses on downloading the required addons for web-application penetration testing.
-###Version:
-#v0.4
-###Homepage:
-#https://github.com/mazen160/Firefox-Security-Toolkit
-###Author:
-#Mazin Ahmed <mazin AT mazinahmed DOT net>
-###################################################################################
+################################################################################
+## Firefox Security Toolkit
+## Description:
+# This script automatically transform Firefox Browser to a penetration testing suite.
+# The script mainly focuses on downloading the required add-ons for web-application penetration testing.
+## Version:
+# v0.5
+## Homepage:
+# https://github.com/mazen160/Firefox-Security-Toolkit
+## Author:
+# Mazin Ahmed <mazin AT mazinahmed DOT net>
+################################################################################
 
 
-logo() { 
+logo() {
 echo '    ______ _              ____                _____                           _  __            ______               __ __ __  _  __ '
 echo '   / ____/(_)_____ ___   / __/____   _  __   / ___/ ___   _____ __  __ _____ (_)/ /_ __  __   /_  __/____   ____   / // //_/ (_)/ /_'
 echo '  / /_   / // ___// _ \ / /_ / __ \ | |/_/   \__ \ / _ \ / ___// / / // ___// // __// / / /    / /  / __ \ / __ \ / // ,<   / // __/'
 echo ' / __/  / // /   /  __// __// /_/ /_>  <    ___/ //  __// /__ / /_/ // /   / // /_ / /_/ /    / /  / /_/ // /_/ // // /| | / // /_  '
 echo '/_/    /_//_/    \___//_/   \____//_/|_|   /____/ \___/ \___/ \__,_//_/   /_/ \__/ \__, /    /_/   \____/ \____//_//_/ |_|/_/ \__/  '
 echo '                                                                                  /____/                                            '
-echo -e "  _               __  __           _            _    _                        _  "
-echo -e " | |__  _   _ _  |  \/  | __ _ ___(_)_ __      / \  | |__  _ __ ___   ___  __| | "
-echo -e " | '_ \| | | (_) | |\/| |/ _\` |_  / | '_ \    / _ \ | '_ \| '_ \` _ \ / _ \/ _\` | "
-echo -e " | |_) | |_| |_  | |  | | (_| |/ /| | | | |  / ___ \| | | | | | | | |  __/ (_| | "
-echo -e " |_.__/ \__, (_) |_|  |_|\__,_/___|_|_| |_| /_/   \_\_| |_|_| |_| |_|\___|\__,_| "
-echo -e "        |___/                                                                    "
-echo -e "v0.4"
-echo -e "\t\t\t\t\twww.mazinahmed.net"
-echo -e "\t\t\t\t\ttwitter.com/mazen160"
-echo -e "\t\t\t\t\tae.linkedin.com/pub/mazin-ahmed/86/795/629"
-echo -e "\n\n"
+echo "  _               __  __           _            _    _                        _  "
+echo " | |__  _   _ _  |  \/  | __ _ ___(_)_ __      / \  | |__  _ __ ___   ___  __| | "
+echo " | '_ \| | | (_) | |\/| |/ _\` |_  / | '_ \    / _ \ | '_ \| '_ \` _ \ / _ \/ _\` | "
+echo " | |_) | |_| |_  | |  | | (_| |/ /| | | | |  / ___ \| | | | | | | | |  __/ (_| | "
+echo " |_.__/ \__, (_) |_|  |_|\__,_/___|_|_| |_| /_/   \_\_| |_|_| |_| |_|\___|\__,_| "
+echo "        |___/                                                                    "
+echo "v0.5"
+echo "www.mazinahmed.net"
+echo "twitter.com/mazen160"
 }
 
 logo
 
 welcome() {
 echo -e "\n\n"
-echo -e "Usage:\n\t bash $0 run\n\n"
-echo -e '[%%] Available Addons:'	
-echo '# Cookie Export/Import
-# Cookie Manager 
-# Copy as Plain Text
-# Crypto Fox
-# CSRF-Finder
-# Disable WebRTC
-# FireBug
-# Fireforce
-# FlagFox
-# Foxy Proxy
-# HackBar
-# Live HTTP Headers
-# Multi Fox
-# PassiveRecon
-# Right-Click XSS
-# Tamper Data
-# User Agent Switcher
-# Wappalyzer
-# Web Developer
+echo -e "Usage:\n\t"
+echo -e "bash $0 run"
+echo -e "\n"
+echo -e '[%%] Available Add-ons:'
+echo '* Advanced Dork
+* Cookie Export/Import
+* Cookie Manager+
+* Copy as Plain Text
+* Crypto Fox
+* CSRF-Finder
+* Disable WebRTC
+* FireBug
+* Fireforce
+* FlagFox
+* Foxy Proxy
+* Greasemonkey
+* HackBar
+* IP Address and Domain Information
+* JavaScript Deobfuscator
+* Live HTTP Headers
+* PassiveRecon
+* Regular Expressions Tester
+* Right-Click XSS
+* Tamper Data
+* User Agent Switcher
+* Wappalyzer
+* Web Developer
 '
-echo -e '[%%] Additions Features:'
-echo -e '# Downloading Burp Suite Certificate'
-echo -e '# Downloading a large user-agent list for User-Agent Swithcer'
+echo '[%%] Additions & Features:'
+echo '* Downloading Burp Suite certificate'
+echo '* Downloading a large user-agent list for User-Agent Switcher'
 echo -e "\n\n"
 echo "[$] Legal Disclaimer: Usage of Firefox Security Toolkit for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program"
-
 }
 
 if [[ $1 != 'run' ]];then
-	welcome
-	exit
+  welcome
+  exit 0
+else
+  echo -en "\n\n[#] Click [Enter] to start. "; read -r
 fi
 
-burp_cert() {
-	wget "http://127.0.0.1:$burp_port/cert" -o /dev/null -O "$scriptpath/cacert.der" ; if [ -s "$scriptpath/cacert.der" ] ; then echo -e "[*] Burp Suite certificate has been downloaded, and can be found at [$scriptpath/cacert.der]."; else echo "[!]Error: Firefox Security Toolkit was not able to download Burp Suite certificate, you need to do this task manually." ; fi
-}
-	
-##Checking whether Firefox is installed.
+# checking whether Firefox is installed.
 if ! [ -f /usr/bin/firefox ]; then
 echo -e "[*] Firefox does not seem to be installed.\n[*]Quitting..."
-exit
+exit 1
 fi
 
-
-echo -en "[#] Click [Enter] to start. "; read -r
-
-##Creating a tmp directory.
+# creating a tmp directory.
 scriptpath=$(mktemp -d)
 echo -e "[*] Created a tmp directory at [$scriptpath]."
 
-##Inserting the "Installation is Finished page" into $scriptpath
+# inserting a "Installation is Finished" page into $scriptpath.
 echo '<!DOCTYPE HTML><html><center><head><h1>Installation is Finished</h1></head><body><p><h2>You can close Firefox.</h2><h3><i>Firefox Security Toolkit</i></h3></p></body></center></html>' > "$scriptpath/.installation_finished.html"
 
+# checks whether the user would like to download Burp Suite certificate.
+echo -n "[@] Would you like to download Burp Suite certificate? [y/n]. (Note: Burp Suite should be running in your machine): "; read -r burp_cert_answer
+  burp_cert_answer=$(echo -n "$burp_cert_answer" | tr '[:upper:]' '[:lower:]')
+  if [[ ( $burp_cert_answer == 'y' ) || ( $burp_cert_answer == 'yes' ) ]];then
+    echo -n "[@] Enter Burp Suite proxy listener's port (Default: 8080): "; read -r burp_port
+    if [[ $burp_port == '' ]]; then
+      burp_port='8080'
+    fi
+    wget "http://127.0.0.1:$burp_port/cert" -o /dev/null -O "$scriptpath/cacert.der"
+    if [ -s "$scriptpath/cacert.der" ];then
+      echo -e "[*] Burp Suite certificate has been downloaded, and can be found at [$scriptpath/cacert.der]."
+    else
+      echo "[!]Error: Firefox Security Toolkit was not able to download Burp Suite certificate, you need to do this task manually."
+    fi
+  fi
 
-##Asks about whether the user would like to download Burpsuite Certificate.
-echo -n "[@] Would you like to download Burp Suite Certificate? [y/n]. (Note that Burp Suite should be running in your machine): "; read -r burp_cert_answer
-	if [[ ( $burp_cert_answer == 'y' ) || ( $burp_cert_answer == 'Y' ) || ( $burp_cert_answer == 'yes' ) ]];then
+# downloading packages.
+echo -e "[*] Downloading Add-ons."
 
-		echo -n "[@] Enter Burp Suite proxy listener's port (Default: 8080): "; read -r burp_port; if [[ $burp_port == '' ]]; then burp_port='8080' ; fi
-		burp_cert
-	fi
-	
-####Downloading packages.
-echo -e "[*] Downloading Addons."
-
-#Copy as Plain Text
+# Copy as Plain Text
 wget "https://addons.mozilla.org/firefox/downloads/latest/copy-as-plain-text/addon-344925-latest.xpi" -o /dev/null -O "$scriptpath/copy_as_plain_text.xpi"
 
-#Web Developer
+# Web Developer
 wget "https://addons.mozilla.org/firefox/downloads/latest/web-developer/addon-60-latest.xpi" -o /dev/null -O "$scriptpath/web_developer.xpi"
 
-#Tamper Data
+# Tamper Data
 wget "https://addons.mozilla.org/firefox/downloads/latest/tamper-data/addon-966-latest.xpi" -o /dev/null -O "$scriptpath/tamper_data.xpi"
 
-#User Agent Switcher
+# User-Agent Switcher
 wget "https://addons.mozilla.org/firefox/downloads/latest/user-agent-switcher/addon-59-latest.xpi" -o /dev/null -O "$scriptpath/user_agent_switcher.xpi"
 
-#Right-Click XSS
+# Right-Click XSS
 wget "https://addons.mozilla.org/firefox/downloads/file/215802/rightclickxss-0.2.1-fx.xpi" -o /dev/null -O "$scriptpath/right_click_xss.xpi"
 
-#Foxy Proxy
+# Foxy Proxy
 wget "https://addons.mozilla.org/firefox/downloads/file/319162/foxyproxy_standard-4.5.5-sm+tb+fx.xpi" -o /dev/null -O "$scriptpath/foxy_proxy.xpi"
 
-#HackBar
+# HackBar
 wget "https://addons.mozilla.org/firefox/downloads/latest/3899/addon-3899-latest.xpi" -o /dev/null -O "$scriptpath/hackbar.xpi"
 
-#Wappalyzer
+# Wappalyzer
 wget "https://addons.mozilla.org/firefox/downloads/latest/wappalyzer/addon-10229-latest.xpi" -o /dev/null -O "$scriptpath/wappalyzer.xpi"
 
-#PassiveRecon
+# PassiveRecon
 wget "https://addons.mozilla.org/firefox/downloads/latest/6196/addon-6196-latest.xpi" -o /dev/null -O "$scriptpath/passiverecon.xpi"
 
-#Cookie Manager+
+# Cookie Manager+
 wget "https://addons.mozilla.org/firefox/downloads/latest/92079/addon-92079-latest.xpi" -o /dev/null -O "$scriptpath/cookiemanager+.xpi"
 
-#Cookie Export/Import
+# Cookie Export/Import
 wget "https://addons.mozilla.org/firefox/downloads/latest/344927/addon-344927-latest.xpi" -o /dev/null -O "$scriptpath/cookie_export_import.xpi"
 
-#FlagFox
+# FlagFox
 wget "https://addons.mozilla.org/firefox/downloads/latest/5791/addon-5791-latest.xpi" -o /dev/null -O "$scriptpath/flagfox.xpi"
 
-#Fireforce
+# Fireforce
 wget "https://addons.mozilla.org/firefox/downloads/file/204186/fireforce-2.2-fx.xpi" -o /dev/null -O "$scriptpath/fireforce.xpi"
 
-#CSRF-Finder
+# CSRF-Finder
 wget "https://addons.mozilla.org/firefox/downloads/file/224182/csrf_finder-1.2-fx.xpi" -o /dev/null -O "$scriptpath/csrf_finder.xpi"
 
-#Multi Fox
-wget "https://addons.mozilla.org/firefox/downloads/latest/200283/addon-200283-latest.xpi" -o /dev/null -O "$scriptpath/multifox.xpi"
-
-#FireBug
+# FireBug
 wget "https://addons.mozilla.org/firefox/downloads/latest/1843/addon-1843-latest.xpi" -o /dev/null -O "$scriptpath/firebug.xpi"
 
-#Live HTTP Headers
+# Live HTTP Headers
 wget "https://addons.mozilla.org/firefox/downloads/file/345004/live_http_headers_fixed_by_danyialshahid-0.17.1-signed-sm+fx.xpi" -o /dev/null -O "$scriptpath/live_http_headers.xpi"
 
-#Crypto Fox
-wget "https://addons.mozilla.org/firefox/downloads/file/140447/cryptofox-2.2-fx.xpi" -o /dev/null  -O "$scriptpath/crypto_fox.xpi" 
+# Crypto Fox
+wget "https://addons.mozilla.org/firefox/downloads/file/140447/cryptofox-2.2-fx.xpi" -o /dev/null  -O "$scriptpath/crypto_fox.xpi"
 
-#Disable WebRTC
-wget "https://addons.mozilla.org/firefox/downloads/latest/497366/addon-497366-latest.xpi" -o /dev/null  -O "$scriptpath/disable_webrtc.xpi" 
+# Disable WebRTC
+wget "https://addons.mozilla.org/firefox/downloads/latest/497366/addon-497366-latest.xpi" -o /dev/null  -O "$scriptpath/disable_webrtc.xpi"
 
-###Ask about whether to download user-agent list for User-Agent Switcher addon
-echo -n "[@] Would you like to download user-agent list for User-Agent Switcher Addon? [y/n]"; read -r useragent_list_answer
-	if [[ ( $useragent_list_answer == 'y' ) || ( $useragent_list_answer == 'Y' ) || ( $useragent_list_answer == 'yes' ) ]]; then
-	wget 'http://techpatterns.com/downloads/firefox/useragentswitcher.xml' -o /dev/null -O "$scriptpath/useragentswitcher.xml" ; echo -e "[*]Additional User-Agnets has been downloaded for Default User-Agent Addon, you can import it manually. It can be found at: [$scriptpath/useragentswitcher.xml]."
-	fi
+# JavaScript Deobfuscator
+wget "https://addons.mozilla.org/firefox/downloads/latest/javascript-deobfuscator/addon-10345-latest.xpi" -o /dev/null  -O "$scriptpath/javascript_deobfuscator.xpi"
 
+# IP Address and Domain Information
+wget "https://addons.mozilla.org/firefox/downloads/latest/ip-address-and-domain-info/addon-394670-latest.xpi" -o /dev/null  -O "$scriptpath/ip-address-and-domain-info.xpi"
 
-####Messages.
-echo -e "[*] Downloading addons has been finished.\n";
-echo -en "[@@] Click [Enter] to run Firefox to finish the task. (Note that Firefox will be restarted) "; read -r
-echo -e "[*] Running Firefox to install the addons.\n"
-##Installing The Addons. The process needs to be semi-manually due to Mozilla Firefox security policies.
-#Stopping Firefox if it's running.
+# Greasemonkey
+wget "https://addons.mozilla.org/firefox/downloads/latest/greasemonkey/addon-748-latest.xpi" -o /dev/null  -O "$scriptpath/greasemonkey.xpi"
+
+# Regular Expressions Tester
+wget "https://addons.mozilla.org/firefox/downloads/latest/rext/addon-2077-latest.xpi" -o /dev/null  -O "$scriptpath/regular-expressions-tester.xpi"
+
+# Advanced Dork
+wget "https://addons.mozilla.org/firefox/downloads/latest/advanced-dork/addon-2144-latest.xpi" -o /dev/null  -O "$scriptpath/advanced-dork.xpi"
+
+# checks whether to download user-agent list for User-Agent Switcher add-on.
+echo -n "[@] Would you like to download user-agent list for User-Agent Switcher add-on? [y/n]"; read -r useragent_list_answer
+  useragent_list_answer=$(echo -n "$useragent_list_answer" | tr '[:upper:]' '[:lower:]')
+  if [[ ( $useragent_list_answer == 'y' ) || ( $useragent_list_answer == 'yes' ) ]]; then
+    wget 'http://techpatterns.com/downloads/firefox/useragentswitcher.xml' -o /dev/null -O "$scriptpath/useragentswitcher.xml"
+    echo -e "[*]Additional user-agents has been downloaded for \"User-Agent Switcher\" add-on, you can import it manually. It can be found at: [$scriptpath/useragentswitcher.xml]."
+  fi
+
+# messages.
+echo -e "[*] Downloading add-ons completed.\n";
+echo -en "[@@] Click [Enter] to run Firefox to perform the task. (Note: Firefox will be restarted) "; read -r
+echo -e "[*] Running Firefox to install the add-ons.\n"
+
+# installing the add-ons.
+# the process needs to be semi-manually due to Mozilla Firefox security policies.
+
+# stopping Firefox if it's running.
 killall firefox &> /dev/null
 #Running it again.
 /usr/bin/firefox "$scriptpath/"*.xpi "$scriptpath/.installation_finished.html" &> /dev/null
 ####
 
-##In case you need to delete the tmp directory, uncomment the following line.
+# in case you need to delete the tmp directory, uncomment the following line.
 #rm -rf "$scriptpath"; echo -e "[*]Deleted the tmp directory."
 echo -e "[**] Firefox Security Toolkit is finished\n"
 echo -e "Have a nice day! - Mazin Ahmed"
-########################################################################
+
+# END #
